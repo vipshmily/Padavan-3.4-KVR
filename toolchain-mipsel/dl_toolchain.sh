a44215c25c803c1e7495682b7a63cb13fb403960
vipshmily <1539755232@qq.com>
2022-11-14 22:25:05 +0800

    update dl_toolchain.sh

--- toolchain-mipsel/dl_toolchain.sh
+++ toolchain-mipsel/dl_toolchain.sh
@@ -2,10 +2,10 @@
 
 DIR="toolchain-3.4.x"
 DL_NAME="mipsel-linux-uclibc.tar.xz"
-DL_URL="https://github.com/hanwckf/padavan-toolchain/releases/download/v1.1/$DL_NAME"
+DL_URL="https://github.com/TurBoTse/padavan-toolchain/releases/download/3.4_kernal/$DL_NAME"
 
 if [ -d $DIR ]; then
-	echo "$DIR exists!"
+	echo "$DIR already exists!"
 	exit
 fi
 
