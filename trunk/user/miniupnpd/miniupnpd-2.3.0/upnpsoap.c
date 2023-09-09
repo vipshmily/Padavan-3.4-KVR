d618e71b7807b1c66ac3292562be3f82bd2e8396
vipshmily <85991289+vipshmily@users.noreply.github.com>
2023-09-09 15:49:43 +0800

    zerotier：update

--- trunk/user/miniupnpd/miniupnpd-2.3.0/upnpsoap.c
+++ trunk/user/miniupnpd/miniupnpd-2.3.0/upnpsoap.c
@@ -347,7 +347,7 @@ GetExternalIPAddress(struct upnphttp * h, const char * action, const char * ns)
 				ext_if_name);
 			ext_ip_addr[0] = '\0';
 		} else if (addr_is_reserved(&addr)) {
-			syslog(LOG_NOTICE, "private/reserved address %s is not suitable for external IP", ext_ip_addr);
+			syslog(LOG_DEBUG, "private/reserved address %s is not suitable for external IP", ext_ip_addr);
 			ext_ip_addr[0] = '\0';
 		}
 	}
--- trunk/user/zerotier/zerotier.sh
+++ trunk/user/zerotier/zerotier.sh
@@ -105,6 +105,9 @@ rules() {
 	iptables -A FORWARD -i $zt0 -j ACCEPT
 	if [ $nat_enable -eq 1 ]; then
 		iptables -t nat -A POSTROUTING -o $zt0 -j MASQUERADE
+		while [ "$(ip route | grep "dev $zt0  proto kernel" | awk '{print $1}')" = "" ]; do
+		    sleep 1
+		done
 		ip_segment="$(ip route | grep "dev $zt0  proto kernel" | awk '{print $1}')"
 		iptables -t nat -A POSTROUTING -s $ip_segment -j MASQUERADE
 		zero_route "add"
@@ -115,11 +118,13 @@ rules() {
 
 del_rules() {
 	zt0=$(ifconfig | grep zt | awk '{print $1}')
-	ip_segment=`ip route | grep "dev $zt0  proto" | awk '{print $1}'`
-	iptables -D FORWARD -i $zt0 -j ACCEPT 2>/dev/null
-	iptables -D FORWARD -o $zt0 -j ACCEPT 2>/dev/null
-	iptables -D FORWARD -i $zt0 -o $zt0 -j ACCEPT
+	ip_segment=`ip route | grep "dev $zt0  proto kernel" | awk '{print $1}'`
+#	iptables -D FORWARD -i $zt0 -j ACCEPT 2>/dev/null
+#	iptables -D FORWARD -o $zt0 -j ACCEPT 2>/dev/null
+#	iptables -D FORWARD -i $zt0 -o $zt0 -j ACCEPT
 	iptables -D INPUT -i $zt0 -j ACCEPT 2>/dev/null
+	iptables -D FORWARD -i $zt0 -o $zt0 -j ACCEPT 2>/dev/null
+	iptables -D FORWARD -i $zt0 -j ACCEPT 2>/dev/null
 	iptables -t nat -D POSTROUTING -o $zt0 -j MASQUERADE 2>/dev/null
 	iptables -t nat -D POSTROUTING -s $ip_segment -j MASQUERADE 2>/dev/null
 }
@@ -152,7 +157,7 @@ start_zero() {
 kill_z() {
 	zerotier_process=$(pidof zerotier-one)
 	if [ -n "$zerotier_process" ]; then
-		logger -t "ZEROTIER" "关闭进程..."
+		logger -t "zerotier" "关闭进程..."
 		killall zerotier-one >/dev/null 2>&1
 		kill -9 "$zerotier_process" >/dev/null 2>&1
 	fi
