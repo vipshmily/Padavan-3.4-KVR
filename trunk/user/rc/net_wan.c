85ae35872e49378bfed24635305b3a0adb955bbf
vipshmily <85991289+vipshmily@users.noreply.github.com>
2023-09-09 10:22:53 +0800

    user: add pppoe-relay

--- trunk/user/rc/net_wan.c
+++ trunk/user/rc/net_wan.c
@@ -1027,6 +1027,8 @@ start_wan(void)
 #endif
 		}
 	}
+
+	set_passthrough_pppoe(1);
 }
 
 static void
@@ -1083,6 +1085,7 @@ stop_wan(void)
 		"udpxy",
 		"udhcpc",
 		"zcip",
+		"pppoe-relay",
 		"detect_wan",
 		NULL
 	};
@@ -1112,6 +1115,7 @@ stop_wan(void)
 
 	stop_auth_eapol();
 	stop_auth_kabinet();
+	set_passthrough_pppoe(0);
 
 	kill_services(svcs_wan, 3, 1);
 
