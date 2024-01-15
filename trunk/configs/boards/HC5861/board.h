0d1b6ec798203b43ad77d6d67767d3e663db9290
vipshmily <85991289+vipshmily@users.noreply.github.com>
2024-01-15 20:24:59 +0800

    add MI-3MI and HC5861

---
+++ trunk/configs/boards/HC5861/board.h
@@ -0,0 +1,33 @@
+/* HC5861 */
+
+#define BOARD_PID		"HC5861"
+#define BOARD_NAME		"HC5861"
+#define BOARD_DESC		"HiWiFi HC5861 Wireless Router"
+#define BOARD_VENDOR_NAME	"HiWiFi"
+#define BOARD_VENDOR_URL	"http://www.hiwifi.com/"
+#define BOARD_MODEL_URL		"http://www.hiwifi.com/"
+#define BOARD_BOOT_TIME		25
+#define BOARD_FLASH_TIME	120
+#undef BOARD_GPIO_BTN_FN1
+#define BOARD_GPIO_BTN_RESET	12
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_SW2G	44
+#define BOARD_GPIO_LED_SW5G	40
+#define BOARD_GPIO_LED_POWER	31 // NA
+#define BOARD_GPIO_LED_LAN    7 //SYS
+#define BOARD_GPIO_LED_WAN	  8 //WORK
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	1
+#define BOARD_NUM_ANT_5G_RX	1
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_NUM_UPHY_USB3	1
+#define BOARD_GPIO_PWR_USB	41
+#define BOARD_GPIO_PWR_USB_ON	0
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0
