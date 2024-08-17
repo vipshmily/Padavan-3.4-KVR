5256e6de2af73f90f737ec9569da521a34451897
vipshmily <85991289+vipshmily@users.noreply.github.com>
2024-08-17 12:05:10 +0800

    Some updates and fixes

--- trunk/user/scripts/reset_ss.sh
+++ trunk/user/scripts/reset_ss.sh
@@ -39,36 +39,51 @@ echo 1        > /proc/sys/net/ipv4/conf/default/rp_filter
 echo 1        > /proc/sys/net/ipv4/conf/eth2/rp_filter
 
 # conntrack
-echo 0        > /proc/sys/net/netfilter/nf_conntrack_checksum
-echo 1        > /proc/sys/net/netfilter/nf_conntrack_tcp_be_liberal
-echo 300      > /proc/sys/net/netfilter/nf_conntrack_generic_timeout
-echo 3600     > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_established
-echo 30       > /proc/sys/net/netfilter/nf_conntrack_icmp_timeout
-echo 50       > /proc/sys/net/netfilter/nf_conntrack_udp_timeout
+echo 0 > /proc/sys/net/netfilter/nf_conntrack_checksum
+echo 1 > /proc/sys/net/netfilter/nf_conntrack_tcp_be_liberal
+echo 1 > /proc/sys/net/netfilter/nf_conntrack_tcp_loose
+echo 15 > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_syn_sent
+echo 15 > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_syn_recv
+echo 1800 > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_established
+echo 30 > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_fin_wait
+echo 30 > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_close_wait
+echo 30 > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_last_ack
+echo 30 > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_time_wait
+echo 30 > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_close
+echo 30 > /proc/sys/net/netfilter/nf_conntrack_udp_timeout
+echo 120 > /proc/sys/net/netfilter/nf_conntrack_udp_timeout_stream
+echo 30 > /proc/sys/net/netfilter/nf_conntrack_icmp_timeout
+echo 600 > /proc/sys/net/netfilter/nf_conntrack_generic_timeout
 
-# tcp performance (enable it only for if you see very high tcp connections)
-#echo 1 > /proc/sys/net/netfilter/nf_conntrack_tcp_loose
-#echo 10 > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_syn_sent
-#echo 10 > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_syn_recv
-
-#echo 15 > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_fin_wait
-#echo 15 > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_close_wait
-#echo 15 > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_last_ack
-#echo 15 > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_time_wait
-#echo 15 > /proc/sys/net/netfilter/nf_conntrack_tcp_timeout_close
-#echo 15 > /proc/sys/net/netfilter/nf_conntrack_udp_timeout
-#echo 15 > /proc/sys/net/netfilter/nf_conntrack_udp_timeout_stream
-#echo 15 > /proc/sys/net/netfilter/nf_conntrack_icmp_timeout
-
-#echo 1 > /proc/sys/net/ipv4/tcp_tw_recycle
-#echo 65536 > /proc/sys/net/ipv4/tcp_max_orphans
-
-# icmp for ipv4/ipv6
-echo 0 > /proc/sys/net/ipv4/icmp_ratelimit
-echo 0 > /proc/sys/net/ipv4/icmp_ratemask
-echo 0 > /proc/sys/net/ipv6/icmp/ratelimit
-echo 10 > /proc/sys/net/netfilter/nf_conntrack_icmp_timeout
-echo 10 > /proc/sys/net/netfilter/nf_conntrack_icmpv6_timeout
+# CPU IRQ
+set_rps_rfs() {
+    echo f >/proc/irq/11/smp_affinity
+    echo f >/proc/irq/12/smp_affinity
+    for device in $(ls /sys/class/net); do
+        echo f >/sys/class/net/$device/queues/rx-0/rps_cpus
+        echo 32768 >/sys/class/net/$device/queues/rx-0/rps_flow_cnt
+    done
+    echo 32768 >/proc/sys/net/core/rps_sock_flow_entries
+}
+get_rps_rfs() {
+    cat /proc/irq/11/smp_affinity
+    cat /proc/irq/12/smp_affinity
+    for device in $(ls /sys/class/net); do
+        printf "%-10s %-5s %-10s\n" "$device" "$(cat /sys/class/net/$device/queues/rx-0/rps_cpus)" "$(cat /sys/class/net/$device/queues/rx-0/rps_flow_cnt)"
+    done
+    cat /proc/sys/net/core/rps_sock_flow_entries
+}
+case $1 in
+get)
+    get_rps_rfs
+    ;;
+set)
+    set_rps_rfs
+    ;;
+*)
+    get_rps_rfs
+    ;;
+esac
 
 # panic
 echo 1        > /proc/sys/kernel/panic
